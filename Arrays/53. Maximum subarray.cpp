61f1f125cd564c0ff38c94eb44cf23eaecb31db3
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-01-07 01:02:55 +0530

    Add solution for Maximum Subarray problem
    Implemented Kadane's algorithm to find the maximum subarray sum.

---
+++ Arrays/53. Maximum subarray.cpp
@@ -0,0 +1,25 @@
+// LeetCode 53 : Maximum subarray
+// Approach: Kadane's algorithm
+// Time complexity : Best- O(n), Space: O(1)
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+        if (sum < 0) {
+            sum = 0;
+        }
+        }
+        return maxi;
+    }
+};
