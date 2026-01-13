c511547dd0b803de65177a05fceb707cecbf9496
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-01-13 19:44:19 +0530

    Create 204.Count pimes.cpp

---
+++ Arrays/204.Count pimes.cpp
@@ -0,0 +1,23 @@
+// LeetCode 204 : Count Primes
+// Approach: Sieve of Eratosthenes.
+// Time complexity : Best- O(n* log(logn)), Space: O(n)
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count=0;
+        vector<bool> prime(n+1, true);
+
+        prime[0] = prime[1] = false;
+
+        for(int i=2; i<n ; i++){
+            if(prime[i]){
+                count++;
+                for(int j=i*2 ; j<n ; j=j+i){
+                    prime[j]=0;
+                }
+            }
+        }
+        return count;
+    }
+};
