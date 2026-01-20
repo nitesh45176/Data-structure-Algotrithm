4f8dae2ee2e9c50b492352ab28dd76651a63d498
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-01-20 23:56:35 +0530

    Add insertAtHead function for doubly linked list
    Implemented the insertAtHead function to add nodes at the beginning of a doubly linked list.

---
+++ Linked List/InsertingNode at head in DoublyLinked List.cpp
@@ -0,0 +1,54 @@
+
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+      int data;
+      Node* prev;
+      Node* next;
+
+       // Contructor
+    Node(int d)
+    {
+        this->data = d;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+}
+void insertAtHead( Node *&tail,Node *&head, int d)
+{
+    // empty list
+    if (head == NULL)
+    {
+        Node *temp = new Node(d);
+        head = temp;
+        tail=temp;
+    }
+    else
+    {
+
+        Node *temp = new Node(d);
+
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+}
+int main()
+{
+    Node *node1 = new Node(10);
+
+    Node *head = node1;
+    Node *tail = node1;
+
+    print(head);
+
+    insertAtHead(tail,head, 11);
+    print(head);
+
+    insertAtHead(tail,head, 8);
+    print(head);
+
+    insertAtHead(tail,head, 9);
+    print(head);
