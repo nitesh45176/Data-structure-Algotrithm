7d3a8bf07b952fa00f9595796d1f9c63fa4afb06
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-01-22 13:19:28 +0530

    Add recursive solution for reversing linked list
    Implemented recursive solution to reverse a linked list.

---
+++ Linked List/LeetCode/206.Reverse Linked List.cpp
@@ -0,0 +1,21 @@
+// Leetcode 206 : Reverse the Linked List
+// Approach : Recursion
+// Time and sppace complexity:  O(N) , O(N)
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+       //Base case
+       if(head == NULL || head->next==NULL){
+         return head;
+       }
+
+      //recursive call
+       ListNode* newHead = reverseList(head->next);
+       ListNode* front = head->next;
+       front->next = head;
+       head ->next = NULL;
+       return newHead;
+    }
+};
