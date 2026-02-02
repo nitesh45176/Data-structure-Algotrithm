a21e9c7e792510dfba5dad2d00e69bf443da3978
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-02-02 20:49:21 +0530

    Add function to calculate length of a string

---
+++ Strings/1.Length of string.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int getLength(char name[]){
+    int count = 0;
+    for(int i=0; name[i] != '\0'; i++){
+        count++;
+    }
+    return count;
+}
+int main(){
+    char name[20];
+
+    cout<<"Enter your name: ";
+    cin>>name;
+
+    cout<<"Lengh: "<<getLength(name);
+}
