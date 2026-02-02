367da61a124d330d3ee89d35ae5c89dce411df18
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-02-02 22:11:44 +0530

    Implement reverse string function using swapping

---
+++ Strings/2.Reverse string.cpp
@@ -0,0 +1,16 @@
+
+// Leetcode 344: Reverse a string
+// Approach; swapping
+
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int st=0;
+        int e = s.size() -1;
+         while(st<e){
+            swap(s[st++], s[e--]);
+         }
+    }
+}; 
