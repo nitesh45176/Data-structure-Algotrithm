368629a690b797ee37ad4d759b8dc115c42bd114
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-02-03 23:23:17 +0530

    Add function to find words containing a character
    Implemented a function to find words containing a specific character.

---
+++ Strings/4. Find words containing character.cpp
@@ -0,0 +1,18 @@
+
+// Leetcode 2942:  Find words containing character
+// Approach : Traversing
+// TC, SC:   O(n) , O(n)
+
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+        for(int i=0; i<words.size(); i++){
+            if(words[i].find(x) != string::npos){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
