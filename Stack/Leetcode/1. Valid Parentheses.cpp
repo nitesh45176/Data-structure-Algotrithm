aec821dfcd0d6cd27a8a7e989a82bdad61411710
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-02-11 18:03:48 +0530

    Add comments for Valid Parentheses solution
    Added comments explaining the approach and time/space complexity.

---
+++ Stack/Leetcode/1. Valid Parentheses.cpp
@@ -0,0 +1,35 @@
+
+// Leetcode 20:  Valid Parantheses
+// Approach :  matching each brackets
+//  Tc and SC :  O(n)  ,  O(n)
+
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+
+                char ch = st.top();
+                st.pop();
+
+                if(s[i]==')' && ch!='('){
+                    return false;
+                }
+                if(s[i]=='}' && ch!='{'){
+                    return false;
+                }
+                if(s[i]==']' && ch!='['){
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
