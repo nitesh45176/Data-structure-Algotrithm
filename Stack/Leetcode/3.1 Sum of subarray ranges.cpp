7fb24e49d187adaad9956c599680bcb2454eea62
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-02-14 23:51:33 +0530

    Add solution for sum of subarray ranges
    Implemented a solution for calculating the sum of subarray ranges using a stack approach.

---
+++ Stack/Leetcode/3.1 Sum of subarray ranges.cpp
@@ -0,0 +1,23 @@
+
+// Leetcode 2104 :  Sum of subarray ranges
+// Approach :  Stack
+//  Tc and SC :  O(n)  ,  O(1)
+
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        long long sum=0;
+        for(int i=0; i<nums.size(); i++){
+            int largest = nums[i];
+            int smallest = nums[i];
+
+            for(int j=i+1; j<nums.size(); j++){
+                largest = max(largest, nums[j]);
+                smallest = min(smallest, nums[j]);
+
+                sum = sum + (largest - smallest);
+            }
+        }
+        return sum;
+    }
+};
