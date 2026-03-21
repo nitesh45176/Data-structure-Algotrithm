adeb2735231966c0f2af7d31a2665a47e4a4b307
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-03-22 00:26:04 +0530

    Remove height function from Solution class
    Removed private height function from Solution class.

--- BinaryTrees/Balanced-Binary-tree-2.cpp
+++ BinaryTrees/Balanced-Binary-tree-2.cpp
@@ -15,19 +15,7 @@
  * };
  */
 class Solution {
- private:
-    int height(TreeNode* node){
-        if(node == NULL){
-            return 0;
-        }
-        
-        int left = height(node->left);
-        int right = height(node->right);
-        
-        int ans = max(left, right) + 1;
-        
-        return ans;
-    }
+
 public:
     pair<bool, int> isBalancedFast(TreeNode* root){
          // base case
