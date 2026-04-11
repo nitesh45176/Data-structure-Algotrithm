62aefc629398893df5863a543d713907c11e096b
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-04-11 12:18:08 +0530

    Add validation function for binary search tree
    Implement recursive function to validate if a binary tree is a BST.

--- Binary Search Tree/6. Validate BST.cpp
+++ Binary Search Tree/6. Validate BST.cpp
@@ -2,3 +2,26 @@
 //Leetcode 98 : Validate BST
 //Approach : Recursive
 // TC & SC : O(n) , O(n)
+
+
+class Solution {
+public:
+    bool isBST(TreeNode* root, long long min, long long max){
+        if(root==NULL){
+            return true;
+        }
+
+        if(root->val > min && root->val< max){
+            bool left = isBST(root->left, min, root->val);
+            bool right = isBST(root->right, root->val, max);
+            return left && right;
+        }
+        else{
+            return false;
+        }
+    }
+    bool isValidBST(TreeNode* root) {
+        return isBST(root, LLONG_MIN, LLONG_MAX);
+
+    }
+};
