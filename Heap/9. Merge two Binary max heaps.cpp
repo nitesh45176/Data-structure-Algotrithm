62a7bafefdaf6b52591e8e58d0fe1a70efd2a63f
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-06-15 12:41:39 +0530

    Implement merging of two binary max heaps
    Fix heapify function to correctly swap elements and update the largest index. Implement merging of two binary max heaps into a single heap.

--- Heap/9. Merge two Binary max heaps.cpp
+++ Heap/9. Merge two Binary max heaps.cpp
@@ -1,3 +1,41 @@
+class Solution {
+  public:
+// 0-based indexing
+   void heapify(vector<int> &arr, int i ,int n){
 
+     int largest = i;
+     int left = 2*i +1;
+     int right = 2*i + 2;
 
-hlo
+     if(left < n && arr[largest] < arr[left]){
+       largest = left;
+     }
+     if(right < n && arr[largest] < arr[right]){
+       largest = right;
+     }
+
+     if(largest != i){
+       swap(arr[largest], arr[i]);
+       heapify(arr, i, n);
+     }
+   }
+    
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        // step 1:- merge two arrays into one
+      vector<int> ans;
+      for(auto i:a){
+        ans.push_back(i);
+      }
+      for(auto i:b){
+        ans.push_back(i);
+      }
+
+      // step 2:- heapify the merged array (0- based indexing)
+      int n = ans.size();
+      for(int i=n/2-1; i<=0; i--){
+          heapify(ans, i, n);
+      }
+
+      return ans;
+    }
+};
