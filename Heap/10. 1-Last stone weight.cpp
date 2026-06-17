b666a682dab6ca248486a3b4dea314a7601d7f3d
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-06-17 23:04:29 +0530

    Add brute force solution for Last Stone Weight
    Implement brute force solution for Last Stone Weight problem using sorting.

---
+++ Heap/10. 1-Last stone weight.cpp
@@ -0,0 +1,26 @@
+
+// Leetcode 1046 : Last stone weight
+// Approach : Brute force solution uing sorting
+// TC, SC :-  o(n * nlogn) , o(n)
+
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        while(stones.size() > 1){
+            sort(stones.begin(), stones.end());
+
+            int a = stones.back();
+            stones.pop_back();
+
+            int b = stones.back();
+            stones.pop_back();
+
+            stones.push_back(abs(a-b));
+
+
+        }
+        return stones[0];
+    }
+};
