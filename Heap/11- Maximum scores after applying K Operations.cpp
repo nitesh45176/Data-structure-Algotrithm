e66825f667be2acc54af1b419f6dbe6c26581736
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-06-18 20:47:25 +0530

    Add maxKelements function for K operations on scores
    Implement maxKelements function to calculate maximum scores after K operations using a max heap.

---
+++ Heap/11- Maximum scores after applying K Operations.cpp
@@ -0,0 +1,25 @@
+
+
+
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        
+        long long sum = 0;
+        priority_queue<int> pq(begin(nums), end(nums));   // creating maxHeap and implementing Heapiffy
+
+
+       while(k--){
+          int maxEl = pq.top();
+           pq.pop();
+
+           sum += maxEl;
+        
+            maxEl = ceil(maxEl/3.0);
+
+            pq.push(maxEl);
+       }
+       return sum;
+
+    }
+};
