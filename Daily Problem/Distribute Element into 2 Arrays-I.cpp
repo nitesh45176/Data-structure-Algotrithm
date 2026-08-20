c479bc12482f85ccea8798270d064193f170ea83
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-08-20 17:20:11 +0530

    Add solution to distribute elements into two arrays
    Implemented a solution to distribute elements from an input array into two separate arrays based on their values.

---
+++ Daily Problem/Distribute Element into 2 Arrays-I.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> arr1;
+        vector<int> arr2;
+
+        int n1=0, n2=0;
+
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+
+        for(int i=2; i<n; i++){
+            if(arr1.back() > arr2.back()){
+                arr1.push_back(nums[i]);
+            }
+            else{
+                arr2.push_back(nums[i]);
+            }
+        }
+    vector<int> result;
+    int k=0;
+    for(auto x: arr1){
+        result.push_back(x);
+    }
+    for(auto x: arr2){
+        result.push_back(x);
+    }
+    return result;
+    }
+};
