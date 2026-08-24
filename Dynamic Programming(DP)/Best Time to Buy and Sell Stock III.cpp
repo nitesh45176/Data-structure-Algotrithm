d2622a556a1ad81b99f02e70a34ffe0c67daf1af
Nitesh kumar Mishra <nitesh_it_28@msit.in>
2026-08-24 22:02:38 +0530

    Add solution for Best Time to Buy and Sell Stock III
    Implement dynamic programming solution for maximizing profit from buying and selling stock up to two times.

---
+++ Dynamic Programming(DP)/Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(
+    n+1, vector<vector<int>>(2, vector<int>(3, 0))
+);
+
+
+
+        for(int i = n-1; i>=0; i--){
+            for(int buy = 0; buy <=1; buy++){
+                for(int cap=1; cap <=2; cap++){
+                    int profit = 0;
+                    if(buy){
+                        int take = -prices[i] + dp[i+1][0][cap];
+                        int notTake = 0 + dp[i+1][1][cap];
+
+                        profit = max(take, notTake);
+                    }
+                    else{
+                        int take = prices[i] + dp[i+1][1][cap-1];
+                        int notTake = 0 + dp[i+1][0][cap];
+
+                        profit = max(take, notTake);
+                    }
+                    dp[i][buy][cap] = profit;
+                }
+                    }
+        }
+    return dp[0][1][2];
+    }
+};
